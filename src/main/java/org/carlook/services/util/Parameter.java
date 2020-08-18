cca3b121e069d90f494cb50011e1e27b28691b29
Andre Merckel <andre.merckel@me.com>
2020-08-18 15:47:35 +0200

    outsourcing kennzeichen

--- src/main/java/org/carlook/services/util/Parameter.java
+++ src/main/java/org/carlook/services/util/Parameter.java
@@ -5,9 +5,18 @@ public class Parameter {
     public static final String COPY_RIGHT = "- " + Parameter.COMPANY + ", coded by: André Merckel, Martin Müller -";
 
     public static final String COMPANY_NAME = "CarLook";
-    public static final String COMPANY_LEGAL_FORM = "Ltd.";
-    public static final String COMPANY = COMPANY_NAME + " " + COMPANY_LEGAL_FORM;
+    public static final String COMPANY_LEGAL_FORM = "Ldt.";
     public static final String PROJECT_NAME = "SE2_Hausarbeit";
     public static final String COMPANY_MAIL_DOMAIN = "de";
+
+    public static final String KENNZEICHEN_ORT = "SU";
+    public static final String KENNZEICHEN_KUERZEL = "CL";
+
+
+
+
+
     public static final String COMPANY_MAIL_ADDRESS = ("@" + COMPANY_NAME.toLowerCase() + "." + COMPANY_MAIL_DOMAIN).trim();
+    public static final String COMPANY = COMPANY_NAME + " " + COMPANY_LEGAL_FORM;
+    public static final String KENNZEICHEN = (KENNZEICHEN_ORT + "-" + KENNZEICHEN_KUERZEL + "-").toUpperCase();
 }
