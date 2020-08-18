d75979baebe1e18f348c8c000487ac4d2c2ec482
Andre Merckel <andre.merckel@me.com>
2020-08-18 15:25:41 +0200

    change DB Schema name into "SE2_Hausarbeit"

--- src/main/java/org/carlook/services/util/Parameter.java
+++ src/main/java/org/carlook/services/util/Parameter.java
@@ -4,8 +4,8 @@ public class Parameter {
 
     public static final String COMPANY_NAME = "CarLook";
     public static final String COMPANY_LEGAL_FORM = "Ltd.";
-    public static final String COMPANY= COMPANY_NAME + " " + COMPANY_LEGAL_FORM;
-    public static final String PROJECT_NAME = "SE2 - Hausarbeit " + COMPANY_NAME + " " + COMPANY_LEGAL_FORM;
+    public static final String COMPANY = COMPANY_NAME + " " + COMPANY_LEGAL_FORM;
+    public static final String PROJECT_NAME = "SE2_Hausarbeit";
     public static final String COMPANY_MAIL_DOMAIN = "de";
     public static final String COMPANY_MAIL_ADDRESS = ("@" + COMPANY_NAME.toLowerCase() + "." + COMPANY_MAIL_DOMAIN).trim();
 }
