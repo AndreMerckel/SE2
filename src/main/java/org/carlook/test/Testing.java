b1166aa1d158179bc7560ec204932e3744c8a01f
martinmuller <martin.mueller.de@outlook.com>
2020-09-07 17:13:55 +0200

    Selenium Test added

---
+++ src/main/java/org/carlook/test/Testing.java
@@ -0,0 +1,35 @@
+package org.carlook.test;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Testing{
+    @Test
+    public void testSeleniumJava() throws Exception {
+
+        // Point to Path for chromedriver on your Computer
+        String driverpath = "/Users/martinmuller/Downloads/chromedriver85";
+
+        System.setProperty("webdriver.chrome.driver", driverpath);
+
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 4);
+
+        driver.get("http://localhost:8080/SE2_war_exploded/");
+
+        WebElement email = driver.findElement(By.id("Email"));
+        email.sendKeys("j@j.com");
+        WebElement password = driver.findElement(By.id("Password"));
+        password.sendKeys("asdf");
+        driver.findElement(By.className("loginButton")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("header_main_menuBox_headLabel")));
+
+    }
+
+}
