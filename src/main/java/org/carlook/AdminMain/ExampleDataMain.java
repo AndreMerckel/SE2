cef60c3ded57acf052ec0278262ae6cdde7e9135
Andre Merckel <andre.merckel@me.com>
2020-09-19 04:04:04 +0200

    ExampleData optimizing

---
+++ src/main/java/org/carlook/AdminMain/ExampleDataMain.java
@@ -0,0 +1,13 @@
+package org.carlook.AdminMain;
+
+import org.carlook.controller.UserControl;
+import org.carlook.controller.exception.DatabaseException;
+import org.carlook.model.dao.LogDAO;
+import org.carlook.services.db.exampleData.UtilNewUserData;
+
+public class ExampleDataMain {
+
+    public static void main(String[] args) {
+        UtilNewUserData.printFahrzeugData();
+    }
+}
